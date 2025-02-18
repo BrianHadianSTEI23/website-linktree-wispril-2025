0c9e771df8e983f26dc76acb3d27c5adf805c27c
brihadianSTEI <19623254@std.stei.itb.ac.id>
2025-02-18 17:16:52 +0700

    feat : home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,181 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const isOpen = searchParams.get("modal") === "true";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const openModal = () => router.push("?modal=true", { scroll: false });
+  const closeModal = () => router.push("/", { scroll: false });
+
+  const toggleContent = () => {
+    router.push(isOpen ? "/" : "/?modal=true", { scroll: false });
+  }
+
+  return (
+    <div className="bg-[#003092] min-h-screen w-full sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        {/* container */}
+        <div className="relative flex-col justify-center items-center">
+
+          {/* Menu icon */}
+          <div className="absolute flex justify-center items-center left-[77%]">
+            <button onClick={(toggleContent)}>
+              <Image
+                src="/menu.png"
+                alt="menu icon"
+                width={30}
+                height={30}
+              />
+            </button>
+          </div>
+
+          {/* Header Image */}
+          <div className="flex justify-center items-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/itbtree.png"
+              alt="itbtree"
+              width={75}
+              height={75}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          {/* Caption Image */}
+          <div className="flex justify-center items-center pt-4 text-4xl font-sans font-semibold">
+            Parade Wisuda ITB
+          </div>
+
+          {/* description */}
+          <div className="flex justify-center text-center items-center w-full">
+            <div className="w-[60%] p-4">
+              Parade Wisuda ITB merupakan rangkaian acara untuk menyambut mahasiswa ITB yang telah menyelesaikan
+              masa studinya di ITB dan lulus sebelum April 2025.
+            </div>
+          </div>
+
+          {/* shareable media */}
+          <div className="flex justify-center items-center w-full pb-4">
+            {/* container */}
+            <div className="flex justify-around items-center w-[30%]">
+
+              {/* twitter */}
+              <div>
+                <Image
+                  src="/x.png"
+                  alt="x"
+                  width={30}
+                  height={0}
+                />
+              </div>
+
+              {/* youtube */}
+              <div>
+                <Image
+                  src="/youtube.png"
+                  alt="x"
+                  width={30}
+                  height={0}
+                />
+              </div>
+
+              {/* instagram */}
+              <div>
+                <Image
+                  src="/instagram.png"
+                  alt="x"
+                  width={30}
+                  height={0}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* main content */}
+          <div className="flex justify-center items-center">
+            {/* content container */}
+            <div className="w-[60%] grid grid-cols-1 gap-4 justify-center items-center">
+              {/* Parade wisuda juli 2020 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Juli 2020
+                </a>
+              </div>
+
+              {/* Parade wisuda oktober 2020 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Oktober 2020
+                </a>
+              </div>
+
+              {/* Parade wisuda April 2021 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda April 2021
+                </a>
+              </div>
+
+              {/* Parade wisuda Oktober 2021 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Oktober 2021
+                </a>
+              </div>
+
+              {/* parade wisuda april 2022 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda April 2022
+                </a>
+              </div>
+
+              {/* parade wisuda oktober 2022 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Oktober 2022
+                </a>
+              </div>
+
+              {/* parade wisuda April 2023 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda April 2023
+                </a>
+              </div>
+              
+              {/* parade wisuda oktober 2022 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Oktober 2023
+                </a>
+              </div>
+
+              {/* parade wisuda April 2024 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda April 2024
+                </a>
+              </div>
+
+              {/* parade wisuda oktober 2024 */}
+              <div className="flex justify-center items-center w-full bg-[#00879E] rounded-3xl border-2 border-[#FFAB5B] border-solid text-[#FFF2DB] p-6">
+                <a href="">
+                  Parade Wisuda Oktober 2024
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          {/* rendered content */}
+          {isOpen && (
+            <div className="text-lg">
+              This content is controlled by the button you've clicked
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+   </div>
   );
 }
