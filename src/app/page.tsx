092f025a6bf0ae9144691c586255d0fec64b7bcc
brihadianSTEI <19623254@std.stei.itb.ac.id>
2025-02-23 09:56:42 +0700

    feat : 'more' pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,22 @@
 
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+
 
 export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const isOpen = searchParams.get("modal") === "true";
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+  // const isOpen = searchParams.get("modal") === "true";
 
-  const openModal = () => router.push("?modal=true", { scroll: false });
-  const closeModal = () => router.push("/", { scroll: false });
+  // const openModal = () => router.push("?modal=true", { scroll: false });
+  // const closeModal = () => router.push("/", { scroll: false });
 
-  const toggleContent = () => {
-    router.push(isOpen ? "/" : "/?modal=true", { scroll: false });
-  }
+  // const toggleContent = () => {
+  //   router.push(isOpen ? "/" : "/?modal=true", { scroll: false });
+  // }
 
   return (
     <div className="bg-[#003092] min-h-screen w-full sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -22,7 +26,9 @@ export default function Home() {
 
           {/* Menu icon */}
           <div className="absolute flex justify-center items-center left-[77%]">
-            <button onClick={(toggleContent)}>
+            <button onClick={() => setIsOverlayOpen(true)}
+              // className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              >
               <Image
                 src="/menu.png"
                 alt="menu icon"
@@ -170,11 +176,170 @@ export default function Home() {
           </div>
 
           {/* rendered content */}
-          {isOpen && (
-            <div className="text-lg">
-              This content is controlled by the button you've clicked
+
+          {isOverlayOpen && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+              <div className="bg-white p-6 rounded-lg shadow-lg w-[40%] text-center text-black h-[90%]">
+                {/* title and close button container*/}
+                <div className="relative flex items-center justify-center h-[10%]">
+                  {/* title */}
+                  <div className="flex w-70% text-xl font-semibold">
+                    Share ITBtree
+                  </div>
+
+                  {/* close button */}
+                  <div className="absolute right-2">
+                    <Image
+                      onClick={() => setIsOverlayOpen(false)}
+                      src={"/exit.png"}
+                      alt="exit button"
+                      width={25}
+                      height={25}
+                    />
+                  </div>
+                </div>
+
+                {/* linktree logo */}
+                <div className="h-[45%] items-center justify-center bg-[#372739] rounded-lg">
+                  {/* itbtree logo */}
+                  <div className="h-[70%] flex justify-center items-center">
+                    <Image
+                      src={"/itbtree.png"}
+                      alt="itbtree logo"
+                      width={50}
+                      height={50}
+                    />
+                  </div>
+                  {/* caption */}
+                  <div className="flex items-start justify-center text-2xl tracking-tight text-white">
+                    @ITBtree
+                  </div>
+                </div>
+
+                {/* media socials */}
+                <div className="flex justify-center items-center h-[15%] my-2">
+                  {/* copy link */}
+                  <div className="p-2">
+                    {/* logo */}
+                    <div className="flex justify-center items-center p-2">
+                      <Image
+                        src={"/copy-link.png"}
+                        alt="copy current link"
+                        width={40}
+                        height={40}
+                      />
+                    </div>
+
+                    {/* caption */}
+                    <div className="font-semibold tracking-tight">
+                      Copy Link
+                    </div>
+                  </div>
+
+                      {/*  instagram */}
+                    <div className="p-2">
+                      {/* logo */}
+                      <div className="flex justify-center items-center p-2 bg-black rounded-lg bg-center">
+                        <Image
+                          src={"/instagram.png"}
+                          alt="go to instagram page"
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+
+                      {/* caption */}
+                      <div className="font-semibold tracking-tight">
+                        Instagram
+                      </div>
+                    </div>
+
+                    {/*  x */}
+                    <div className="p-2">
+                      {/* logo */}
+                      <div className="flex justify-center items-center p-2 bg-black rounded-lg bg-center">
+                        <Image
+                          src={"/x.png"}
+                          alt="go to x page"
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+
+                      {/* caption */}
+                      <div className="font-semibold tracking-tight">
+                        X
+                      </div>
+                    </div>
+
+                    {/*  youtube */}
+                    <div className="p-2">
+                      {/* logo */}
+                      <div className="flex justify-center items-center p-2 bg-black rounded-lg bg-center">
+                        <Image
+                          src={"/youtube.png"}
+                          alt="go to youtube page"
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+
+                      {/* caption */}
+                      <div className="font-semibold tracking-tight">
+                        Youtube
+                      </div>
+
+                    </div>
+                </div>
+
+                {/* persuasive content */}
+                <div className="text-start pb-2">
+                  {/* title */}
+                  <div className="text-lg font-bold tracking-tighter">
+                    Join Parade Wisuda ITB
+                  </div>
+
+                  {/* caption */}
+                  <div>
+                    Get your own free ITBtree. The only link in bio trusted by 50M+ people.
+                  </div>
+
+                  {/* buttons */}
+                  <div className="flex justify-between py-2 h-[30%]">
+                    {/* sign up */}
+                    <div className="w-0.5 flex flex-1 justify-center items-center text-center bg-black text-white mx-2 rounded-2xl" >
+                      Sign Up Free
+                    </div>
+
+                    {/* find out more */}
+                    <div className="w-0.5 flex flex-1 justify-center items-center text-center border-black border border-solid mr-2 rounded-2xl">
+                      Find Out More
+                    </div>
+                  </div>
+
+                </div>
+
+                {/* report link */}
+                <div className="flex items-center h-[5%]">
+                  {/* flag icon */}
+                  <div className="flex justify-center items-center w-[10%]">
+                    <Image
+                      src={"/flag.png"}
+                      alt={"flag"}
+                      width={25}
+                      height={25}
+                    />
+                  </div>
+
+                  {/* caption */}
+                  <div className="text-opacity-10 flex items-start justify-center">
+                    Report ITBtree
+                  </div>
+                </div>
+                
+              </div>
             </div>
-          )}
+          )}  
         </div>
    </div>
   );
